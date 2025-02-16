42b088a9e034287e85270b2db1c1339b853f11b5
Baptiste Geisenberger <baptistegeisenberger@hotmail.com>
2025-02-16 17:17:54 +0100

    Ran models, Error analysis script

--- PromptEngineering/test.py
+++ PromptEngineering/test.py
@@ -1,61 +1,25 @@
 import json
 import os
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 
-def load_claims(file_path: str) -> list:
+docs_results = "docs/results/results_with_cells_llama3.2:latest_test_examples_1695_zero_shot_html.json"
+
+def load_predictions(file_path):
     """
-    Load a JSON file from the given path and return a list of dictionaries with 'claim' and 'ID' 
-    for each element.
-    """
-    all_claims = []
+    Load a JSON file containing a list of dictionaries into a Python list of dictionaries.
     
-    try:
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-        
-        id_counter = 1
-        
-        for item in data:
-            claim_with_resp = {
-                "ID": id_counter,
-                "claim": item.get("claim", ""),
-            }
-            
-            id_counter += 1
-            
-            all_claims.append(claim_with_resp)
-    
-    except Exception as e:
-        print(f"Error loading file: {e}")
-    
-    return all_claims
-
-import os
-
-def list_files_in_folder(path_name: str) -> list:
-    """
-    Given a folder path, return a list of all file names in the folder.
-
-    Parameters:
-    - path_name: The path to the folder.
-
-    Returns:
-    - A list of file names in the folder.
     """
     try:
-        # List all files in the given folder
-        file_names = [f for f in os.listdir(path_name) if os.path.isfile(os.path.join(path_name, f))]
-        return file_names
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+        return data
     except Exception as e:
-        print(f"Error listing files in folder: {e}")
+        print(f"Error loading JSON file from {file_path}: {e}")
         return []
-
-
-path_name = "docs/results/"
-l = list_files_in_folder(path_name)
-
-print(l)
-for file in l:
-    d = load_claims("docs/results/" + file)
-    print(file, len(d))
-
-print(len(load_claims("PromptEngineering/results_20250212/results_CodeGeneration_test_examples_zero_shot_naturalized_phi4.json")))
+    
+claims = load_predictions(docs_results)
+for claim in claims:
+    print(claim['claim'])
+    print()
